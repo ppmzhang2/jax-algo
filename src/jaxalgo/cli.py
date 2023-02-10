1d564a4fb3063cd96072b0820fa86766a1d8333b
ZHANG, Meng <stall-breezes.0t@icloud.com>
2023-02-10 22:14:40 +1300

    Add Fine-Tuning Process and Revision

--- src/jaxalgo/cli.py
+++ src/jaxalgo/cli.py
@@ -1,12 +1,8 @@
 """All commands here."""
 import click
 
-from jaxalgo.serv.resnet import train_resnet
-from jaxalgo.serv.yolov3 import coco_annot_to_csv
-from jaxalgo.serv.yolov3 import create_labels
-from jaxalgo.serv.yolov3 import db_reset
-from jaxalgo.serv.yolov3 import load_coco_annot
-from jaxalgo.serv.yolov3 import train_yolo
+from jaxalgo.serv import resnet as resnet_cli
+from jaxalgo.serv import yolov3 as yolov3_cli
 
 
 @click.group()
@@ -14,11 +10,12 @@ def yolov3() -> None:
     """All YOLOv3 commands. Including data preparation, training model, etc."""
 
 
-yolov3.add_command(coco_annot_to_csv)
-yolov3.add_command(create_labels)
-yolov3.add_command(db_reset)
-yolov3.add_command(load_coco_annot)
-yolov3.add_command(train_yolo)
+yolov3.add_command(yolov3_cli.coco_annot_to_csv)
+yolov3.add_command(yolov3_cli.create_labels)
+yolov3.add_command(yolov3_cli.db_reset)
+yolov3.add_command(yolov3_cli.load_coco_annot)
+yolov3.add_command(yolov3_cli.train)
+yolov3.add_command(yolov3_cli.tuning)
 
 
 @click.group()
@@ -26,4 +23,4 @@ def resnet() -> None:
     """All clicks here."""
 
 
-resnet.add_command(train_resnet)
+resnet.add_command(resnet_cli.train_resnet)
